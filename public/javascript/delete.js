1931265d35e948ed717025a20663722edd535d83
windsail <windsailg@gmail.com>
2020-09-01 02:44:23 +0800

    optimization script position

--- public/javascript/delete.js
+++
@@ -1,7 +0,0 @@
-const restaurantContainer = document.querySelector('.card-columns')
-restaurantContainer.addEventListener('click', (e) => {
-  if (e.target.matches('.delete__restaurant__btn')) {
-    const id = e.target.dataset.id
-    document.delete_form.action = `./restaurant/${id}/delete`
-  }
-})
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,23 +1,7 @@
-const restaurantContainer = document.querySelector('.card-columns')
-
-restaurantContainer.addEventListener('click', (e) => {
+// delete restaurant  function
+document.querySelector('body').addEventListener('click', (e) => {
   if (e.target.matches('.delete__restaurant__btn')) {
     const id = e.target.dataset.id
-    document.delete_form.action = `./restaurant/${id}/delete`
+    document.delete_form.action = `/restaurant/${id}/delete`
   }
-})
-
-const ratingText = document.querySelector('.rating_number')
-const ratingRanger = document.querySelector('.custom-range')
-
-ratingRanger.addEventListener('input', () => {
-  ratingText.value = ratingRanger.value + ' 顆星'
-})
-
-const imageInput = document.querySelector('.image_input')
-const imagShow = document.querySelector('.image_show')
-
-imageInput.addEventListener('input', () => {
-  imagShow.src = `${imageInput.value}`
-  imagShow.parentElement.classList.remove('d-none')
 })
